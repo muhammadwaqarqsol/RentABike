03df2911ce5cfc19ad47230f7b3a3a504d3efa0a
Muhammad Waqar <muhammadwaqar.business@gmail.com>
2023-08-16 18:39:54 +0500

    single bike rent and return scenerio with testing

--- contracts/NFTRent.sol
+++ contracts/NFTRent.sol
@@ -1,85 +1,191 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.9;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./IERC4907.sol";
 
-contract MyToken is IERC4907, ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
-    
+contract Rentabike is  ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+
+    uint public chargeRate =500000000000000000;
+
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("MyToken", "MTK") {}
+    constructor() ERC721("MyToken", "MTK")
+    {}
+    
+    //Mapping for keeping userride active when rented otherwise false
+    mapping(address=>bool) public activeRide;
 
-    mapping(uint256=>address) creatorNFT;
+    //Mapping to track startingTime for a ride
+    mapping(address=>uint) public startTime;
 
-    mapping(uint256=>address) userofTokenID;
+    //mapping to keep track of rentedbikes
+    mapping (uint256=>address) public rentedNFT;
 
-    mapping(uint256=>uint64) tokenExpires;
-
-    mapping(address=>uint64) rentExpire;
-
-    // mapping(uint256=>address) rentedNFT;
-
-    function setUser(uint256 tokenId,address user,uint64 expires) public virtual  {
-        require(_isApprovedOrOwner(msg.sender,tokenId),"ERC:721 Transfer is not A caller Nor Approved");
-        userofTokenID[tokenId]=user;
-        rentExpire[user]=expires;
-        tokenExpires[tokenId]=expires;
-        _transfer(creatorNFT[tokenId],user,tokenId);
-    }
-
-    function userOf(uint256 tokenId,address user) public view  virtual   returns(address){
-        if(uint256(rentExpire[user])>=block.timestamp){
-            return userofTokenID[tokenId];
-        }else{
-            return address(0);
-        }
-    }
-
-    function userExpires(uint256 tokenId) public view virtual returns (uint256){
-        return tokenExpires[tokenId];
-    }
+    //Mapping to keep track of Listed Bikes for Rent
+    mapping(uint256=>bool) public nftListed;
 
 
+    //Minting bike only owner
     function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
+        require(to!=address(0),"Cannot Mint to zero address");
+        // Check if URI is not empty
+        require(bytes(uri).length > 0, "URI cannot be empty"); 
+        //Increment token count for each nft uniquely
         _tokenIdCounter.increment();
+        //Save Current token counter
+        uint256 tokenId = _tokenIdCounter.current();
+        //mintNFt to specific address
         _safeMint(to, tokenId);
+        //set the URI of the NFT
         _setTokenURI(tokenId, uri);
-        creatorNFT[tokenId]=msg.sender;
+    }
+    
+    function listNFT(uint256 tokenId)public{
+        //check if the sender has the same token ownership
+        require(msg.sender==ownerOf(tokenId),"You dont have that NFT token!");
+        //list nft for the Rent
+        nftListed[tokenId]=true;
     }
 
-    function userOf(uint256 tokenId) external view returns (address){
-        return userofTokenID[tokenId];
-            }
 
-    // function userExpires(address user) external view returns (uint256){
-    //     return tokenidExpires[user];
-    // }
+    
+    function rentBike(address walletAddress,uint256 tokenId,uint64 starttime)public{
+        require(walletAddress!=address(0),"Cannot Rent to zero Address");
+        require(tokenId!=0,"Bike doesnot Existed");
+        require(starttime!=0,"Start time cannot be zero");
+        //Check If NFT is listed for the Rent
+        require(nftListed[tokenId]==true,"You cannot rent the bike");
+        require(rentedNFT[tokenId]==address(0),"You cannot rent this bike already Rented");
+        require(activeRide[walletAddress]==false,"You already have the Bike Rented");
+        // require(renters[walletAddress].due==0,"Pay Your Dues before Renting");
+        //Set Active ride after renting a bike
+        activeRide[walletAddress]=true;
+       //set starting time for the ride
+        startTime[walletAddress]=starttime;
+        // canRent[walletAddress]=false;
+        //set NFT for rent to keep them from transfering
+        rentedNFT[tokenId]=walletAddress;
+    }
+    
 
-    // The following functions are overrides required by Solidity.
+    function returnBike(address walletAddress, uint64 endtime,uint256 tokenId) public payable {
+        require(walletAddress!=address(0),"Cannot Rent to zero Address");
+        require(tokenId!=0,"Bike doesnot Existed");
+        require(endtime!=0,"End time cannot be zero");
+        //check if already have an active Ride
+        require(activeRide[walletAddress] == true, "You haven't rented a bike");
+    
+        // Calculate charges based on rental duration using the separate function
+        uint256 charges = calculateCharges(walletAddress, endtime);
+    
+        // Check if the sent value (msg.value) is sufficient to cover the charges
+        require(msg.value >= charges, "Low balance for checkout");
+    
+        // Calculate remaining amount after deducting charges
+        uint256 remainingAmount = msg.value - charges;
+    
+        // Send the calculated charges to the owner
+        (bool success,) = owner().call{value: charges}("");
+        
+        //check if amount is send to owner
+        require(success, "Failed to send money to owner");
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-       require(msg.sender == owner(),"Only the contract owner can set the user");
+        //after payment setting active ride false
+        activeRide[walletAddress]=false;
+
+        //Removing nft from rent
+        rentedNFT[tokenId]=address(0);
+
+        //Reseting StartTime
+        startTime[walletAddress]=0;
+
+        // Refund the remaining amount to the sender
+        uint256 refundAmount = remainingAmount;
+        //sent back the amount to user
+        payable(msg.sender).transfer(refundAmount);
+    }
+
+    function calculateCharges(address walletAddress, uint64 endtime) public view returns (uint256) {
+        //If active Ride then calculate charges
+        require(activeRide[walletAddress] == true, "You have not rented any bike");
+        //check the starting time for the Ride
+        uint256 starttime = startTime[walletAddress];
+        //startime and Endtime cannot be same
+        require(endtime!=starttime,"Start and End Time Cannot same");
+        require(starttime < endtime, "Start time must be less than end time");
+        //calculate time difference from start and end
+        uint256 calculateTimeInBetween = endtime - starttime;
+        //calculate time in minutes  
+        uint256 timeSpanInMinutes = calculateTimeInBetween / 60;
+        //calculate time increment 30 minutes with perthirty minute .5 eth charges  
+        uint256 thirtyMinuteCharge = (timeSpanInMinutes / 30) * chargeRate; // Charging chargeRate ETH per 30 minutes
+        //return the charge amount
+        return thirtyMinuteCharge;
+    }
+
+    
+    //get contract balance 
+    function balanceof() view public returns(uint){
+        //return the contract balance
+        return address(this).balance;
+    }
+
+    function changePrice(uint256 percentage) public  returns (uint256) {
+        //check percentage greater than zero less than 100
+        require(percentage >= 0 && percentage <= 100, "Percentage out of range");
+    
+        // Convert the percentage to a decimal value
+        uint256 decimalValue = (percentage * 1e18) / 100;
+
+        //store chargeRate
+        chargeRate=decimalValue;
+
+        //return wei value
+        return decimalValue;
+    }
+
+    //withdraw any amount from the contract
+    function withdraw() public onlyOwner { 
+        //check amount to withdraw   
+        uint256 amount = address(this).balance;
+        
+        require(amount > 0, "Nothing to withdraw; contract balance empty");
+        //owner account of contract
+        address _owner = owner();
+        
+        //sent the amount to owner
+        (bool sent, ) = _owner.call{value: amount}("");
+        //check if sent or not
+        require(sent, "Failed to send Ether");
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)internal override(ERC721, ERC721Enumerable){
+
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
+
+        if (rentedNFT[tokenId] != address(0)) {
+        // If rentedNFt[tokenId] is not equal to address(0), prevent the transfer.
+            require(false, "Transfer not allowed");}
     }
+
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        
         super._burn(tokenId);
+    
     }
+
 
     function tokenURI(uint256 tokenId)
         public
         view
-        override(ERC721,ERC721URIStorage)
+        override(ERC721, ERC721URIStorage)
         returns (string memory)
     {
         return super.tokenURI(tokenId);
@@ -93,28 +199,4 @@ contract MyToken is IERC4907, ERC721, ERC721Enumerable, ERC721URIStorage, Ownabl
     {
         return super.supportsInterface(interfaceId);
     }
-    
-
-    // // Override the transfer function
-    // function transfer(address to, uint256 tokenId) public override {
-    // require(msg.sender == creatorNFT[tokenId], "You are not the owner of this NFT");
-    // // Transfer the NFT to the new owner
-    // super.transfer(to, tokenId);
-    // }
-
-    // // Override the safeTransfer function
-    // function safeTransfer(address to, uint256 tokenId, bytes memory data) public override {
-    //  require(msg.sender == creatorNFT[tokenId], "You are not the owner of this NFT");
-
-    // // Transfer the NFT to the new owner
-    // super.safeTransfer(to, tokenId, data);
-    // }
-
-    // // Override the safeTransferFrom function
-    // function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override {
-    //  require(msg.sender == creatorNFT[tokenId], "You are not the owner of this NFT");
-    // // Transfer the NFT to the new owner
-    // super.safeTransferFrom(from, to, tokenId, data);
-
-    // }
 }
--- test/foundry/NFTRentTest.sol
+++ test/foundry/NFTRentTest.sol
@@ -1,129 +1,261 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import "forge-std/Test.sol";
-// import "../../contracts/NFTRent.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "../../contracts/NFTRent.sol";
 
-// contract MarketTest is Test {
+contract BikeRentTest is Test {
 
-//     using OrderTypes for OrderTypes.MakerOrder;
-//     using OrderTypes for OrderTypes.TakerOrder;
+    Rentabike public nft_contract;
 
-//     NFTMarket public _contract;
-//     NFT public nft_contract;
+    uint256 internal ownerPrivateKey;
+    address internal owner;
+    
+    uint256 internal userPrivateKey;
+    address internal user;
 
-//     uint256 internal makerPrivateKey;
-//     address internal maker;
+    function setUp() public {
+        nft_contract = new Rentabike();
 
-//     uint256 internal takerPrivateKey;
-//     address internal taker;
+        ownerPrivateKey = 0xA11CE;
+        owner = vm.addr(ownerPrivateKey);
 
-//     event testAddress (address);
-//     event testMakerOrder (OrderTypes.MakerOrder);
-//     event testTakerOrder (OrderTypes.TakerOrder);
+        userPrivateKey = 0xB0B;
+        user = vm.addr(userPrivateKey);
 
-//     OrderTypes.MakerOrder public tempMakerOrder;
+        nft_contract.transferOwnership(owner);
 
-//     function setUp() public {
-//         _contract = new NFTMarket();
-//         nft_contract = new NFT(address(_contract));
+        vm.deal(user, 2 ether);
+    }
 
-//         makerPrivateKey = 0xA11CE;
-//         maker = vm.addr(makerPrivateKey);
-        
-//         takerPrivateKey = 0xB0B;
-//         taker = vm.addr(takerPrivateKey);
+    function testInitial() public {
+        assertEq(nft_contract.balanceOf(owner), 0);
+    }
 
-//         vm.deal(taker, 2 ether);
+    function testmintNFt() public {
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        vm.stopPrank();
+        assertEq(nft_contract.ownerOf(1), owner);
+        assertEq(nft_contract.totalSupply(),1);
+    }
 
-//         //create a token in nft contract
-//         vm.prank(maker);
-//         nft_contract.createToken("some uri");
+     function testmintNFtZeroAddress() public {
+        vm.startPrank(owner);
+        vm.expectRevert("Cannot Mint to zero address");
+        nft_contract.safeMint(address(0),"abc");
+        vm.expectRevert("URI cannot be empty");
+        nft_contract.safeMint(owner,"");
+        vm.stopPrank();
+    }
 
-//         //create a maker bid
-//         sign();
+    function testRentBikeWithList()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        nft_contract.listNFT(1);
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),true);
+        vm.startPrank(user);
+        nft_contract.rentBike(user,1,1692092111);
+        assertEq(nft_contract.nftListed(1),true);
+    }
 
-//         //create a taker bid
-//     }
+    function testcannotRentWithOutListing()public{
+         vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),false);
+        vm.startPrank(user);
+        vm.expectRevert("You cannot rent the bike");
+        nft_contract.rentBike(user,1,1692092111);
+    }
 
-//     function testInitial() public {
-//         assertEq(nft_contract.contractAddress(), address(_contract));
-//         assertEq(nft_contract.ownerOf(1), maker);
-//         assertEq(nft_contract.balanceOf(maker), 1);
-//     }
+     function testcannotRentWithZeroAddress()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),false);
+        vm.startPrank(user);
+        vm.expectRevert("You cannot rent the bike");
+        nft_contract.rentBike(user,1,1692092111);
+    }
 
-//     function testBuying() public {
-//         emit testAddress(taker);
-//         emit testAddress(maker);
-//         emit testAddress(address(this));
-//         uint256 oldBal = address(this).balance;
-//         vm.startPrank(taker);
-//         OrderTypes.TakerOrder memory takerOrder = OrderTypes.TakerOrder(false, taker, 0.3 ether, 1, 9000);
-//         emit testTakerOrder(takerOrder);
-//         _contract.matchAskWithTakerBid{value: 0.3 ether}(takerOrder, tempMakerOrder);
-//         vm.stopPrank();
+    function testcannotRentWithZeroTokenID()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),false);
+        vm.startPrank(user);
+        vm.expectRevert("Bike doesnot Existed");
+        nft_contract.rentBike(user,0,1692092111);
+    }
 
-//         assertEq(nft_contract.ownerOf(1), taker);
-//         assertEq(address(maker).balance, 0.2 ether);
-//         assertEq(address(this).balance, oldBal + 0.1 ether);
-//     }
+    function testcannotRentWithZeroStartTime()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),false);
+        vm.startPrank(user);
+        vm.expectRevert("Start time cannot be zero");
+        nft_contract.rentBike(user,1,0);
+    }
 
-//     function sign() public returns (uint8 v, bytes32 r, bytes32 s) {
-//         OrderTypes.MakerOrder memory makerOrder = OrderTypes.MakerOrder(true, maker, maker, address(nft_contract), 0.2 ether, 1, 0.1 ether, 6, 0, 0x00, 0x00);
-//         bytes32 digest = getTypedDataHash(makerOrder);
-//         vm.prank(maker);
-//         // emit testAddress(maker);
-//         (v, r, s) = vm.sign(makerPrivateKey, digest);
-//         makerOrder.v = v;
-//         makerOrder.r = r;
-//         makerOrder.s = s;
-//         tempMakerOrder = makerOrder;
-//         emit testMakerOrder(makerOrder);
-//     }
+     function testRentsameBikeTwice()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        nft_contract.listNFT(1);
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),true);
+        vm.startPrank(user);
+        nft_contract.rentBike(user,1,1692092111);
+        assertEq(nft_contract.nftListed(1),true);
+        vm.startPrank(address(3));
+        vm.expectRevert("You cannot rent this bike already Rented");
+        nft_contract.rentBike(address(3),1,1692092111);
+    }
 
-//     function getStructHash(OrderTypes.MakerOrder memory makerOrder) internal pure returns (bytes32) {
-//         return
-//             keccak256(
-//                 abi.encode(
-//                     MakerOrder_TYPEHASH,
-//                     makerOrder.isOrderAsk,
-//                     makerOrder.signer,
-//                     makerOrder.nftContract,
-//                     makerOrder.baseAccount,
-//                     makerOrder.price,
-//                     makerOrder.nonce,
-//                     makerOrder.tokenId
-//                 )
-//             );
-//     }
+    function testbikeReturnWithoutactiveRide() public{
+        vm.startPrank(user);
+        vm.expectRevert("You haven't rented a bike");
+        nft_contract.returnBike{value:1 ether}(user,1692093911,1);
+    }
 
-//     function getTypedDataHash(OrderTypes.MakerOrder memory makerOrder) public view returns (bytes32) {
-//         return
-//             keccak256(
-//                 abi.encodePacked(
-//                     "\x19\x01",
-//                     domainHash(),
-//                     getStructHash(makerOrder)
-//                 )
-//             );
-//     }
+    function testrentBikeWithRetureWithZeroAddress()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        nft_contract.listNFT(1);
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),true);
+        vm.startPrank(user);
+        nft_contract.rentBike(user,1,1692092111);
+        assertEq(nft_contract.rentedNFT(1),user);
+        assertEq(nft_contract.startTime(user),1692092111);
+        vm.expectRevert("Cannot Rent to zero Address");
+        nft_contract.returnBike{value:1 ether}(address(0),1692093911,1);
+        assertEq(address(user).balance,2 ether);
+    }
 
-//     bytes32 public constant MakerOrder_TYPEHASH = keccak256("ListNFT(bool IsOrderAsk,address sender,address collection,address baseAccount,uint price,uint nonce,uint tokenId)");
+     function testrentBikeWithRetureWithZeroTokenId()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        nft_contract.listNFT(1);
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),true);
+        vm.startPrank(user);
+        nft_contract.rentBike(user,1,1692092111);
+        assertEq(nft_contract.rentedNFT(1),user);
+        assertEq(nft_contract.startTime(user),1692092111);
+        vm.expectRevert("Bike doesnot Existed");
+        nft_contract.returnBike{value:1 ether}(user,1692093911,0);
+        assertEq(address(user).balance,2 ether);
+    }
 
-//     function domainHash() internal view returns(bytes32) {
-//         bytes32 hash = keccak256(
-//             abi.encode(
-//                 keccak256(
-//                     "EIP712Domain(string name,string version,address verifyingContract)"
-//                 ),
-//                 keccak256(bytes("MarketPlace")),
-//                 keccak256(bytes("1")),
-//                 address(_contract)
-//             )
-//         );
-//         return hash;
-//     }
+     function testrentBikeWithRetureWithZeroEndTime()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        nft_contract.listNFT(1);
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),true);
+        vm.startPrank(user);
+        nft_contract.rentBike(user,1,1692092111);
+        assertEq(nft_contract.rentedNFT(1),user);
+        assertEq(nft_contract.startTime(user),1692092111);
+        vm.expectRevert("End time cannot be zero");
+        nft_contract.returnBike{value:1 ether}(user,0,1);
+        assertEq(address(user).balance,2 ether);
+    }
 
-//     fallback () external payable {}
-//     receive () external payable {}
-// }
+    function testrentBikeWithRetureWithsameTime()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        nft_contract.listNFT(1);
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),true);
+        vm.startPrank(user);
+        nft_contract.rentBike(user,1,1692092111);
+        assertEq(nft_contract.rentedNFT(1),user);
+        assertEq(nft_contract.startTime(user),1692092111);
+        vm.expectRevert("Start and End Time Cannot same");
+        nft_contract.returnBike{value:1 ether}(user,1692092111,1);
+        assertEq(address(user).balance,2 ether);
+    }
+
+     function testrentBikeWithRetureWithstartTimegreaterthanEnd()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        nft_contract.listNFT(1);
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),true);
+        vm.startPrank(user);
+        nft_contract.rentBike(user,1,1692092113);
+        assertEq(nft_contract.rentedNFT(1),user);
+        vm.expectRevert("Start time must be less than end time");
+        nft_contract.returnBike{value:1 ether}(user,1692092111,1);
+        assertEq(address(user).balance,2 ether);
+    }
+
+    function testrentBikeWithReture()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        nft_contract.listNFT(1);
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),true);
+        vm.startPrank(user);
+        nft_contract.rentBike(user,1,1692092111);
+        assertEq(nft_contract.rentedNFT(1),user);
+        assertEq(nft_contract.startTime(user),1692092111);
+        nft_contract.returnBike{value:1 ether}(user,1692093911,1);
+        assertEq(address(owner).balance,0.5 ether);
+        assertEq(address(user).balance,1.5 ether);
+    }
+    
+
+    function testrentedBikeCannotBeTransfer()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        nft_contract.listNFT(1);
+        assertEq(nft_contract.nftListed(1),true);
+        nft_contract.rentBike(user,1,1692092111);
+        assertEq(nft_contract.rentedNFT(1),user);
+        assertEq(nft_contract.startTime(user),1692092111);
+        vm.expectRevert("Transfer not allowed");
+        nft_contract.transferFrom(owner,user,1);
+    }
+
+    function testrentedbikeReturnCanbeTransfered() public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        nft_contract.listNFT(1);
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),true);
+        vm.startPrank(user);
+        nft_contract.rentBike(user,1,1692092111);
+        assertEq(nft_contract.rentedNFT(1),user);
+        assertEq(nft_contract.startTime(user),1692092111);
+        nft_contract.returnBike{value:1 ether}(user,1692093911,1);
+        assertEq(address(owner).balance,0.5 ether);
+        assertEq(address(user).balance,1.5 ether);
+        vm.startPrank(owner);
+        nft_contract.transferFrom(owner,address(3),1);
+        assertEq(nft_contract.ownerOf(1),address(3));
+    }
+
+     function testrentBikeWithReturePriceChange()public{
+        vm.startPrank(owner);
+        nft_contract.safeMint(owner,"abc");
+        nft_contract.listNFT(1);
+        nft_contract.changePrice(20);
+        vm.stopPrank();
+        assertEq(nft_contract.nftListed(1),true);
+        vm.startPrank(user);
+        nft_contract.rentBike(user,1,1692092111);
+        assertEq(nft_contract.rentedNFT(1),user);
+        assertEq(nft_contract.startTime(user),1692092111);
+        nft_contract.returnBike{value:1 ether}(user,1692093911,1);
+        assertEq(address(owner).balance,0.2 ether);
+        assertEq(address(user).balance,1.8 ether);
+    }
+
+  
+}
